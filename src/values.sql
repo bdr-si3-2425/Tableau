fa957462c426360bba7086f7b639e6b243c277cc
Cyril <45211909+Musdevl@users.noreply.github.com>
2025-02-05 09:36:27 +0100

    Update values.sql

--- src/values.sql
+++ src/values.sql
@@ -11,8 +11,8 @@ INSERT INTO Abonnes (id, nom, prenom, email, telephone, adresse, bibliotheque_id
 ('3','Bernard', 'Sophie', 'sophie.bernard@mail.com', '0634567890', '7 boulevard Saint-Michel, Toulouse', 3),
 ('4','Lemoine', 'Jean', 'jean.lemoine@mail.com', '0645678901', '10 place du Marché, Marseille', 4),
 ('5','Garcia', 'Emma', 'emma.garcia@mail.com', '0656789012', '5 rue Victor Hugo, Lille', 5),
-('6','Dulon','Alex','alex.dulon@mail.com','0602345678','15 rue des Dolines, Paris',1)
-('7','Farat','Paul','paul.farat@mail.com','0652771278','15 rue des Palais, Paris',1)
+('6','Dulon','Alex','alex.dulon@mail.com','0602345678','15 rue des Dolines, Paris',1),
+('7','Farat','Paul','paul.farat@mail.com','0652771278','15 rue des Palais, Paris',1);
 
 INSERT INTO Participants (id, nom, prenom, email, telephone) VALUES
 ('1','Lemoine', 'Jean', 'jean.lemoine@mail.com', '0611111176'),
@@ -59,18 +59,18 @@ INSERT INTO Reserve (abonne_id, exemplaire_id, date_reservation) VALUES
 (5, 8, '2025-02-14');
 
 INSERT INTO Loue (abonne_id, exemplaire_id, date_emprunt, date_retour) VALUES
-(5,7,'2025-02-06','2025-02-20',0),
-(3,4,'2025-02-05','2025-02-28',1),
-(4,4,'2025-03-05','2025-03-28',0),
-(6,4,'2025-04-05','2025-03-29',0),
-(7,4,'2025-06-09','2025-07-28',0),
-(2,4,'2025-08-05','2025-08-13',0),
-(1,4,'2025-08-15','2025-09-01',0),
-(6,6,'2025-02-05','2025-02-28',0),
-(2,3,'2025-02-05','2025-02-28',0),
-(7,2,'2025-02-05','2025-02-28',0),
-(6,2,'2025-02-05',NULL,0),
-(4,2,'2025-02-06',NULL,0);
+(5,7,'2025-02-06','2025-02-20'),
+(3,4,'2025-02-05','2025-02-28'),
+(4,4,'2025-03-05','2025-03-28'),
+(6,4,'2025-04-05','2025-03-29'),
+(7,4,'2025-06-09','2025-07-28'),
+(2,4,'2025-08-05','2025-08-13'),
+(1,4,'2025-08-15','2025-09-01'),
+(6,6,'2025-02-05','2025-02-28'),
+(2,3,'2025-02-05','2025-02-28'),
+(7,2,'2025-02-05','2025-02-28'),
+(6,2,'2025-02-05',NULL),
+(4,2,'2025-02-06',NULL);
 
 
 
@@ -96,9 +96,9 @@ INSERT INTO S_abonne (abonne_id, bibliotheque_id, date_abonnement) VALUES
 (4, 4, '2025-01-04'),
 (5, 5, '2025-01-05');
 
-INSERT INTO Personnel (id, nom, prenom, email, telephone, adresse, role, bibliotheque_id) VALUES
-(1,'Leroy', 'Camille', 'camille.leroy@mail.com', '0678901234', '3 rue du Savoir, Paris', 'gestionnaire', 1),
-(2,'Dubois', 'Luc', 'luc.dubois@mail.com', '0689012345', '8 place du Marché, Lyon', 'bibliothécaire', 2),
-(3,'Morel', 'Nina', 'nina.morel@mail.com', '0690123456', '12 boulevard Victor, Toulouse', 'administrateur', 3),
-(4,'Faure', 'Pierre', 'pierre.faure@mail.com', '0676543210', '25 rue du Lac, Marseille', 'bibliothécaire', 4),
-(5,'Bertrand', 'Julie', 'julie.bertrand@mail.com', '0665432109', '7 avenue du Nord, Lille', 'gestionnaire', 5);
+INSERT INTO Personnel (nom, prenom, email, telephone, adresse, role, bibliotheque_id) VALUES
+('Leroy', 'Camille', 'camille.leroy@mail.com', '0678901234', '3 rue du Savoir, Paris', 'gestionnaire', 1),
+('Dubois', 'Luc', 'luc.dubois@mail.com', '0689012345', '8 place du Marché, Lyon', 'bibliothécaire', 2),
+('Morel', 'Nina', 'nina.morel@mail.com', '0690123456', '12 boulevard Victor, Toulouse', 'administrateur', 3),
+('Faure', 'Pierre', 'pierre.faure@mail.com', '0676543210', '25 rue du Lac, Marseille', 'bibliothécaire', 4),
+('Bertrand', 'Julie', 'julie.bertrand@mail.com', '0665432109', '7 avenue du Nord, Lille', 'gestionnaire', 5);
